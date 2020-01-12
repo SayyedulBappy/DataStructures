07ae2a81d58462df85a36443572a2989b4f1dc95
“SayyedulBappy” <38081373+SayyedulBappy@users.noreply.github.com>
2020-01-12 22:32:50 +0600

    added option to display Top of stack

--- Stack/StackOperations.py
+++ Stack/StackOperations.py
@@ -11,12 +11,13 @@ for item in stack:
     print(item,end=" ")
 print()
 choice = None
-while(choice!=4):
+while(choice!=5):
     print('###### Stack operations ######')
     print('1. Display the integers')
     print('2. Push integers to stack')
     print('3. Pop integers from stack')
-    print('4. Exit')
+    print('4. Top of stack')
+    print('5. Exit')
 
     choice = input("Enter your choice: ")
     choice = int(choice)
@@ -33,8 +34,10 @@ while(choice!=4):
     elif choice == 3:
         # pop element
         print(stack.pop())
-
     elif choice == 4:
+        top = stack[-1]
+        print(f"Top of stack is {top}")
+    elif choice == 5:
         print("program exited")
         break
     else:

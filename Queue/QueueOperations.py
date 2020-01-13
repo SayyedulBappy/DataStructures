552c7773345e5e738874bde9d0a2d1d817046f48
“SayyedulBappy” <38081373+SayyedulBappy@users.noreply.github.com>
2020-01-13 17:06:53 +0600

    Queue operations implemented from Queue data structure

--- Queue/QueueOperations.py
+++ Queue/QueueOperations.py
@@ -1,16 +1,10 @@
 
+from Queue import Queue
 
-from collections import deque ## for quicker operations
+
 # create queue
 
-print("Enter integers separated by spaces to create an queue: ", end=" ")
-
-queue = deque(map(int,input().strip().split()))
-
-# display the created queue
-
-for item in queue:
-    print(item,end=" ")
+queue = Queue()
 
 choice = None
 while(choice!=6):
@@ -27,25 +21,23 @@ while(choice!=6):
     choice = int(choice)
 
     if choice == 1:
-        for item in queue:
+        for item in queue.items:
             print(item,end=" ")
         
     elif choice == 2:
         # push elements
-        itemToAdd = input("Enter integer to push: ")
-        queue.append(itemToAdd)
+        itemToAdd = input("Enter integer to enqueue: ")
+        queue.enqueue(itemToAdd)
         
     elif choice == 3:
         # remove element
-        print(queue.popleft())
+        print(queue.dequeue())
     elif choice == 4:
         #  element at front
-        front = queue[-1]
-        print(f"Front of queue is {front}")
+        print(f"Front of queue is {queue.front()}")
     elif choice == 5:
         # element at rear
-        rear = queue[0]
-        print(f"Rear of queue is {rear}")
+        print(f"Rear of queue is {queue.rear()}")
     elif choice == 6:
         print("program exited")
         break

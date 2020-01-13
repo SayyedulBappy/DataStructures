543c97fe16543076e84f594ad82d2a9e59270251
“SayyedulBappy” <38081373+SayyedulBappy@users.noreply.github.com>
2020-01-13 17:04:47 +0600

    Array operations implemented from Array data structure

--- Array/ArrayOperations.py
+++ Array/ArrayOperations.py
@@ -1,43 +1,56 @@
 
+from Array import Array 
 # create array
 
-print("Enter integers separated by spaces to create an array: ", end=" ")
-
-arr = list(map(int, input().split()))
+arr = Array()
 
 # display the created array
 
-for item in arr:
-    print(item,end=" ")
-print()
 choice = None
 while(choice!=4):
     print('###### Array operations ######')
-    print('1. display the items')
-    print('2. insert integer to array')
-    print('3. remove integer from array')
-    print('4. Exit')
+    print('1. display item')
+    print('2. append item')
+    print('3. insert item at a specific position')
+    print('4. pop item')
+    print('5. remove item at first occurrance')
+    print('6. index of item at first occurrance')
+    print('7. reverse array')
+    print('8. Exit')
 
     choice = input("Enter your choice: ")
     choice = int(choice)
 
     if choice == 1:
-        for item in arr:
+        for item in arr.items:
             print(item,end=" ")
         
     elif choice == 2:
-        # insert elements
-        itemToAdd = int(input("Enter integer to insert: "))
-        insertPosition = int(input("Enter position to insert: "))
-        arr.insert(insertPosition,itemToAdd)
+        # append elements
+        itemToAdd = int(input("Enter integer to append: "))
+        arr.append(itemToAdd)
         
     elif choice == 3:
+        # insert elements
+        itemToInsert = int(input("Enter integer to insert: "))
+        insertPosition = int(input("Enter position to insert: "))
+        arr.insert(insertPosition,itemToInsert)
+        
+    elif choice == 4:
+        # pop element
+        print(f"popped item is {arr.pop()}")
+    elif choice == 5:
         # remove element
         itemToRemove = int(input("Enter integer to remove: "))
-        index = arr.index(itemToRemove)
-        print(arr.pop(index))
-
-    elif choice == 4:
+        print(arr.remove(itemToRemove))
+    elif choice == 6:
+        # index element
+        item = int(input("Enter integer to find index: "))
+        print(arr.index(item))
+    elif choice == 7:
+        # reverse array
+        arr.reverse()
+    elif choice == 8:
         print("program exited")
         break
     else:
